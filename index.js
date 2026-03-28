c4cf07a6720f26b95cd6e225eb1d5ce857d15383
Juanramosac <juanramosac21@gmail.com>
2026-03-28 14:35:11 -0500

    Lista definitiva de librerias para Chrome

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const fs = require('fs');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
             '--disable-gpu'
-        ],
-        headless: true
+        ]
     }
 });
 
